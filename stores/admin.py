eeb1569e075645f9ff79fa492d1a1fc337f7e9bd
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-09-13 12:48:32 +0300

    django-advanced_02 completed

--- stores/admin.py
+++ stores/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Store
 
 # Register your models here.
-admin.site.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    search_fields = ('name', 'description',)
+    list_display_links = ('name',)
+
+admin.site.register(Store, StoreAdmin)
--- stores/views.py
+++ stores/views.py
@@ -9,5 +9,22 @@ def store_list(request):
     }
     return render(request, 'store_list.html', context)
 
+def store_detail(request, store_slug):
+    store = Store.objects.get(slug=store_slug)
 
+    context = {
+        "store": store,
+    }
+    return render(request, 'store_detail.html', context)
 
+def store_create(request):
+    form = StoreModelForm()
+    if request.method == "POST":
+        form = StoreModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    context = {
+        "form":form,
+    }
+    return render(request, 'store_create.html', context)
